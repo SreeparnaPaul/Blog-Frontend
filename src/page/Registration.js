25405036deffbcd28ab7cecc5d5851238592eb5e
SreeparnaPaul <sreeparnapaul21@gmail.com>
2023-05-01 22:55:06 +0530

    #changes

--- src/page/Registration.js
+++ src/page/Registration.js
@@ -12,7 +12,10 @@ function Registration() {
     const [signInFormData,setSignInFormData]=useState({
         email:"",
         password:"",
-        username:""
+        firstName:"",
+        lastName:"",
+        phoneNumber:"",
+        confirmPassword:"",
     })
     const [showPassword, setShowPassword] = useState(false);
     const history =useHistory()
@@ -78,11 +81,20 @@ function Registration() {
           ) {
             formErrors.email = 'Invalid email address';
           }
+          if (!signInFormData.phoneNumber.trim()) {
+            formErrors.phoneNumber = 'Phone number is required';
+          }
         if (!signInFormData.password.trim()) {
             formErrors.password = 'Password is required';
           } 
-          if (!signInFormData.username.trim()) {
-            formErrors.username = 'Username is required';
+          if (!signInFormData.confirmPassword.trim()) {
+            formErrors.confirmPassword = 'Password is required';
+          } 
+          if (!signInFormData.firstName.trim()) {
+            formErrors.username = 'First name is required';
+          } 
+          if (!signInFormData.lastName.trim()) {
+            formErrors.username = 'Last name is required';
           } 
         return formErrors;
       };
@@ -95,23 +107,36 @@ function Registration() {
       }}>
     <div className="d-flex align-items-center auth px-0">
       <div className="row w-100 mx-0">
-        <div className="col-lg-4 mx-auto">
+        <div className="col-lg-5 mx-auto">
           <div className="auth-form-light text-left py-5 px-4 px-sm-5 text-center" style={{marginTop:"10%"}}>
             <div className='card' style={{display:"flex",justifyContent:"center",marginTop:"5%"}}>
               <div className='card-body'>
                 <Fab color="primary" aria-label="add">
                   <LockIcon />
                 </Fab>
-                <h4 style={{fontFamily:"sans-serif",marginTop:"10px"}}>Sign Up</h4>
-                <div className='mb-4'>
-                  <TextField sx={{ m: 1, width: '90%' }} id="outlined-basic" label="User Name*" type="text" name ="username" variant="outlined" value={signInFormData?.username} onChange={handleChange}/>
-                  {errors?.username && <div style={{color:"red"}}>{errors?.username}</div>}
+                <h4 style={{fontFamily:"sans-serif",marginTop:"10px"}}>Sign up to BlogiFy</h4>
+                <div className='row'>
+                <div className='col-6 mb-4'>
+                  <TextField sx={{ m: 1, width: '90%' }} id="outlined-basic" label="First Name*" type="text" name ="firstName" variant="outlined" value={signInFormData?.firstName} onChange={handleChange}/>
+                  {errors?.firstName && <div style={{color:"red"}}>{errors?.firstName}</div>}
                 </div>
-                <div className='mb-4'>
+                <div className='col-6 mb-4'>
+                <TextField sx={{ m: 1, width: '90%' }} id="outlined-basic" label="Last Name*" type="text" name ="lastName" variant="outlined" value={signInFormData?.lastName} onChange={handleChange}/>
+                {errors?.lastName && <div style={{color:"red"}}>{errors?.lastName}</div>}
+              </div>
+              </div>
+              <div className='row'>
+              <div className='col-6 mb-4'>
                   <TextField sx={{ m: 1, width: '90%' }} id="outlined-basic" label="Email Address*" type="email" name ="email" variant="outlined" value={signInFormData?.email} onChange={handleChange}/>
                   {errors?.email && <div style={{color:"red"}}>{errors?.email}</div>}
                 </div>
-                <div  className='mb-4'>
+                <div className='col-6 mb-4'>
+                <TextField sx={{ m: 1, width: '90%' }} id="outlined-basic" label="Phone Number*" type="number" name ="phoneNumber" variant="outlined" value={signInFormData?.phoneNumber} onChange={handleChange}/>
+                {errors?.phoneNumber && <div style={{color:"red"}}>{errors?.phoneNumber}</div>}
+              </div>
+                </div>
+                <div className='row'>
+              <div className='col-6 mb-4'>
                   <FormControl sx={{ m: 1, width: '90%' }} variant="outlined">
                     <InputLabel htmlFor="outlined-adornment-password">Password*</InputLabel>
                     <OutlinedInput
@@ -139,7 +164,35 @@ function Registration() {
                   </FormControl>
                   {errors?.password && <div style={{color:"red"}}>{errors?.password}</div>}
                 </div>
-               
+                <div className='col-6 mb-4'>
+                <FormControl sx={{ m: 1, width: '90%' }} variant="outlined">
+                  <InputLabel htmlFor="outlined-adornment-password">Confirm Password*</InputLabel>
+                  <OutlinedInput
+                    id="outlined-adornment-password"
+                    type={showPassword ? 'text' : 'password'}
+                    value={signInFormData?.confirmPassword}
+                    name="confirmPassword"
+                    onChange={handleChange}
+                    endAdornment={
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="toggle password visibility"
+                          onClick={handleClickShowPassword}
+                          onMouseDown={(event) => {
+                            event.preventDefault()
+                          }}
+                          edge="end"
+                        >
+                          {showPassword ? <VisibilityOff /> : <Visibility />}
+                        </IconButton>
+                      </InputAdornment>
+                    }
+                    label="Confirm Password*"
+                  />
+                </FormControl>
+                {errors?.confirmPassword && <div style={{color:"red"}}>{errors?.confirmPassword}</div>}
+              </div>
+               </div>
                 <div className='mb-4' style={{fontFamily:"sans-serif"}}>
                     Already have an account? <a href="/signIn"> <b>Click here</b> </a>
                 </div>
